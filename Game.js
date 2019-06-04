07037914ffe2054f73f65e40877a1a7388bbeaf4
Fluteboy123 <35540271+Fluteboy123@users.noreply.github.com>
2019-06-04 10:17:42 +0200

    Add main game code

---
+++ Game.js
@@ -0,0 +1,31 @@
+let gameScene = new Phaser.Scene('Game');
+const config = {
+    type: Phaser.AUTO,
+    height:500,
+    width:800,
+    scene:gameScene,
+    audio:{
+        disableWebAudio: true
+    }
+};
+let game = new Phaser.Game(config);
+
+gameScene.init = function()
+{
+
+};
+
+gameScene.preload = function()
+{
+
+};
+
+gameScene.create = function()
+{
+
+};
+
+gameScene.update = function()
+{
+
+};
