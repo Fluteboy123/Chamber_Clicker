de0945399109fb0527740ddf7e28ed5c19e7cb6d
Fluteboy123 <u1032555@umail.utah.edu>
2019-06-04 12:03:22 +0200

    Simplify the function call implemetation

--- Button.js
+++ Button.js
@@ -1,16 +1,12 @@
 class Button {
-    constructor (scene, x, y, cursorInput) {
+    constructor (scene, x, y, cursorInput, clickFunction) {
         scene.add.existing(this);
         this.x = x;
         this.y = y;
         this.cursorInput = cursorInput;
         this.setImmovable();
         //Function called when the button is clicked
-        this.clickFunction= null;
-    }
-    function setClickFunction(func)
-    {
-        this.clickFunction = func;
+        this.clickFunction= clickFunction;
     }
 
 }
