8cee1bad8f4eed855503a50ed5f5ae6e5c8e9d40
Fluteboy123 <35540271+Fluteboy123@users.noreply.github.com>
2019-06-04 11:40:01 +0200

    Start Background Graphics and Change the title

--- Game.js
+++ Game.js
@@ -2,7 +2,7 @@ let gameScene = new Phaser.Scene('Game');
 const config = {
     type: Phaser.AUTO,
     height:500,
-    width:800,
+    width:1000,
     scene:gameScene,
     audio:{
         disableWebAudio: true
@@ -22,10 +22,80 @@ gameScene.preload = function()
 
 gameScene.create = function()
 {
+    //Positions and colors of the panels
+    this.windowPos = [[0,0],[250,0],[750,0],[750,300]];
+    this.windowColors = [0x88ffff,0xccffff,0xf0f0f0];
+
+    //Graphics
+    this.fillBackground();
+    this.controlPanel = this.add.container(this.windowPos[0][0],this.windowPos[0][1]);
+    this.tweetWall = this.add.container(this.windowPos[1][0],this.windowPos[1][1]);
+    this.upgrades = this.add.container(this.windowPos[2][0],this.windowPos[2][1]);
+    this.eventWindow = this.add.container(this.windowPos[3][0],this.windowPos[3][1]);
+    this.fillControlPanel();
+    this.fillTweetWall();
+    this.fillUpgrades();
+    this.fillEvents();
 
 };
 
-gameScene.update = function()
+gameScene.update = function(time,delta)
 {
+    //Update the event progress
+    if(this.eventWindow.timer>0)
+    {
+        this.eventWindow.timer-=delta;
+        if(this.eventWindow.timer>0)
+        {
 
+        }
+        else
+        {
+
+        }
+    }
+};
+
+gameScene.fillBackground = function()
+{
+    this.backGraphics = this.add.graphics({ fillStyle: { color: this.windowColors[0] } });
+    this.backGraphics.fillRectShape(new Phaser.Geom.Rectangle(this.windowPos[0][0],this.windowPos[0][1],this.windowPos[1][0]-this.windowPos[0][0],500));
+    this.backGraphics.fillRectShape(new Phaser.Geom.Rectangle(this.windowPos[2][0],this.windowPos[2][1],config.width-this.windowPos[2][0],this.windowPos[3][1]-this.windowPos[2][1]));
+    this.backGraphics.fillStyle(this.windowColors[1]);
+    this.backGraphics.fillRectShape(new Phaser.Geom.Rectangle(this.windowPos[1][0],this.windowPos[1][1],this.windowPos[2][0]-this.windowPos[1][0],500));
+    this.backGraphics.fillStyle(this.windowColors[2]);
+    this.backGraphics.fillRectShape(new Phaser.Geom.Rectangle(this.windowPos[3][0],this.windowPos[3][1],config.width-this.windowPos[2][0],config.height-this.windowPos[3][1]));
+};
+
+gameScene.fillControlPanel = function()
+{
+    //Text boxes
+    this.controlPanel.add(this.add.text(10,10,"Tweet",{fill:"#000"}));
+    this.controlPanel.add(this.add.text(10,config.height-100,"Popularity",{fill:"#000"}));
+    this.controlPanel.add(this.add.text(10,config.height-50,"Followers",{fill:"#000"}));
+    //Text boxes that hold the game scores in them
+    this.controlPanel.popularityLabel = this.add.text(10,config.height-75,"0",{fill:"#000"});
+    this.controlPanel.followerLabel = this.add.text(10,config.height-25,"0",{fill:"#000"});
+    this.controlPanel.add(this.controlPanel.popularityLabel);
+    this.controlPanel.add(this.controlPanel.followerLabel);
+};
+gameScene.fillTweetWall = function()
+{
+    //List of all tweet sprites currently on the wall
+    this.tweetWall.currentTweets = [];
+};
+gameScene.fillUpgrades = function()
+{
+    this.upgrades.add(this.add.text(10,10,"Upgrades",{fill:"#000"}));
+};
+gameScene.fillEvents = function()
+{
+    /*startTime and timer represent time in milliseconds
+    *startTime represents how much time the player was given to respond to an event before it disappears
+    * timer represents how much time is left. If timer is negative or zero, there is no current event.
+    * When one event ends, the next event in the queue will run
+    */
+    this.eventWindow.startTime = 10000;
+    this.eventWindow.timer = 0;
+    this.eventWindow.queue = [];
 };
